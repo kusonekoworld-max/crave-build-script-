549677bb399f02739b02126174b146cc1c871ddc
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-07-25 14:26:21 +0400

    Update crave command URL in build_queue.sh

--- build_queue.sh
+++ build_queue.sh
@@ -31,7 +31,7 @@ while [ $ATTEMPT -le $MAX_ATTEMPTS ]; do
     print_text "          attempting build queue ~$((ATTEMPT + 1))..."
 
     # Run the crave command
-    crave run --projectID 93 --no-patch -- "curl -sf https://raw.githubusercontent.com/nuruszama/crave/lineage-23.2/crave_run.sh | bash" 2>&1 | tee "$LOG_FILE"
+    crave run --projectID 93 --no-patch -- "curl -sf https://raw.githubusercontent.com/nuruszama/crave/creek/crave_run.sh | bash" 2>&1 | tee "$LOG_FILE"
 
     # Capture the pipeline status thanks to set -o pipefail
     CRAVE_STATUS=${PIPESTATUS[0]}
