4061cea434b17797b35fd19d02cd71eb0faabc60
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-07-27 16:20:30 +0400

    Update build_queue.sh

--- build_queue.sh
+++ build_queue.sh
@@ -12,12 +12,7 @@ fi
 # source .env file and print a welcome message.
 source .env
 
-print_text () {
-echo "-------------------------------------------------"
-echo "${1}"
-}
-
-print_text "          Building $ROM_NAME for $DEVICE"
+echo ">> Building $ROM_NAME for $DEVICE"
 
 # set the container log file
 LOG_FILE="crave_build.log"
@@ -25,11 +20,11 @@ rm -f "$LOG_FILE"
 
 # crave queue and retry logic
 MAX_ATTEMPTS=2
-ATTEMPT=0
+ATTEMPT=1
 DELAY_TIME="1m" # 1 minutes delay
 
 while [ $ATTEMPT -le $MAX_ATTEMPTS ]; do
-    print_text "          attempting build queue ~$((ATTEMPT))..."
+    echo ">> attempting build queue ~$((ATTEMPT))..."
 
     # Run the crave command
     crave run --projectID 93 --no-patch -- "curl -sf https://raw.githubusercontent.com/nuruszama/crave/creek/crave_run.sh | bash" 2>&1 | tee "$LOG_FILE"
@@ -44,22 +39,22 @@ while [ $ATTEMPT -le $MAX_ATTEMPTS ]; do
         exit 130 # ⚠️ Build Queue Cancelled
 
     else
-        print_text "             Rejected with exit code $CRAVE_STATUS."
+        echo ">> Rejected with exit code $CRAVE_STATUS."
 
         if [ ! -f "$LOG_FILE" ]; then
-            print_text " Build script failed to start! Check the setup"
+            echo ">> Build script failed to start! Check the setup"
             exit 1
         fi
 
         if grep -q "Setting up workspace" "$LOG_FILE"; then
-            print_text "    build failed.. inspect the logs"
+            echo ">> build failed.. inspect the logs"
             exit 1
         else
             if [ $ATTEMPT -lt $MAX_ATTEMPTS ]; then
                 ((ATTEMPT++)) # Safely move to next attempt loop
                 sleep $DELAY_TIME
             else
-                print_text "  Build terminated with ${ATTEMPT} attempts."
+                echo ">> Build terminated with ${ATTEMPT} attempts."
                 exit 1
             fi
         fi
