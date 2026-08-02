06d2ee625a5f6efc0eec1f38bbae7f1b6af58f4c
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-08-02 23:17:43 +0400

    Update paths in vendorextract.sh script

--- local/vendorextract.sh
+++ local/vendorextract.sh
@@ -3,9 +3,9 @@ echo ""
 echo "extracting vendor tree"
 
 # define directories
-DEVICE_TREE="/home/creek/android/lineage/device/xiaomi/creek"
-TOOLS="/home/creek/android/lineage/tools/extract-utils"
-DUMP="/home/creek/android/lineage/device/xiaomi/creek-dump"
+DEVICE_TREE="device/xiaomi/creek"
+TOOLS="tools/extract-utils"
+DUMP="device/xiaomi/creek-dump"
 
 # get into device tree directory
 cd "$DEVICE_TREE" || { echo  "Error: Cannot access device tree folder"; exit 1; }
