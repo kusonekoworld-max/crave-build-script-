6d4664f5fb2cdc92c9fe8361bb704c7d07832228
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-08-12 15:23:30 +0400

    Update start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -14,13 +14,13 @@ export SKIP_ABI_CHECKS=true
 export WITH_DEXPREOPT=true
 
 # remove device tree
-rm -rf vendor/lineage
-rm -rf hardware/qcom-caf/common
+rm -rf .repo/local_manifests
+rm -rf device
+rm -rf vendor
 rm -rf device/xiaomi/creek
 rm -rf device/xiaomi/creek-kernel
 rm -rf vendor/xiaomi/creek
-rm -rf device/xiaomi/sepolicy
-rm -rf .repo/local_manifests
+
 
 # re-initialize the lineage source
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs --depth=1
