7c455f87d242afa4a47d817bde517516ef05290e
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-08-12 16:29:21 +0400

    Fix build log variable assignment

--- start_build.sh
+++ start_build.sh
@@ -5,7 +5,6 @@ clear
 rm -rf extract_log.txt
 
 # Define some requirements
-BUILD_LOG="creek_build_$(date +%s).log"
 
 export BUILD_USERNAME="a.s.k"
 export BUILD_HOSTNAME="thazz"
@@ -42,4 +41,4 @@ breakfast creek eng
 make installclean
 
 # start building
-m bacon 2>&1 | tee "$BUILD_LOG"
+m bacon
