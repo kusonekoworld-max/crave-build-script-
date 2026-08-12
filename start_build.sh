3fc4f8a72c925f18159ab5572b5c49f785a7987e
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-08-12 15:20:31 +0400

    Update start_build.sh

--- start_build.sh
+++ start_build.sh
@@ -20,51 +20,16 @@ rm -rf device/xiaomi/creek
 rm -rf device/xiaomi/creek-kernel
 rm -rf vendor/xiaomi/creek
 rm -rf device/xiaomi/sepolicy
-rm -rf .repo/local_manifests/
+rm -rf .repo/local_manifests
 
 # re-initialize the lineage source
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs --depth=1
 
 #clone local manifest
-git clone https://github.com/nuruszama/LineageOS.git -b 16 --depth=1 .repo/local_manifests
+git clone https://github.com/XiaomiCreek/LineageOS.git -b 16 --depth=1 .repo/local_manifests
 
 # resync the repo source
 repo sync -j16 --force-sync
-
-# ROM source patches
-
-color="\033[0;32m"
-end="\033[0m"
-
-echo "----------------------------------------------------"
-echo -e "${color}Starting hardware HAL sync...${end}"
-echo ""
-
-# Define directories and their matching repo details
-rm -rf hardware/qcom-caf/common
-git clone --depth 1 -b lineage-23.2 https://github.com/sapphire-sm6225/android_hardware_qcom-caf_common.git hardware/qcom-caf/common
-
-rm -rf hardware/qcom-caf/sm6225
-
-git clone --depth 1 -b lineage-22.2-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_agm.git hardware/qcom-caf/sm6225/audio/agm
-
-git clone --depth 1 -b 16-qpr2 https://github.com/XiaomiCreek/hardware_qcom-caf_sm6225_audio_pal.git hardware/qcom-caf/sm6225/audio/pal
-
-git clone --depth 1 -b lineage-23.2-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_data-ipa-cfg-mgr.git hardware/qcom-caf/sm6225/data-ipa-cfg-mgr
-
-git clone --depth 1 -b lineage-23.2-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_dataipa.git hardware/qcom-caf/sm6225/dataipa
-
-git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/hardware_qcom_display.git hardware/qcom-caf/sm6225/display
-
-git clone --depth 1 -b lineage-23.2-caf-sm6225 https://github.com/sapphire-sm6225/hardware_qcom_media.git hardware/qcom-caf/sm6225/media
-
-git clone --depth 1 -b 16-qpr2 https://github.com/XiaomiCreek/hardware_qcom-caf_sm6225_audio_primary-hal.git hardware/qcom-caf/sm6225/audio/primary-hal
-
-rm -rf device/qcom/sepolicy_vndr/sm6225
-git clone --depth 1 -b 16-qpr2 https://github.com/XiaomiCreek/android_device_qcom_sepolicy_vndr_sm6225.git device/qcom/sepolicy_vndr/sm6225
-
-rm -rf vendor/lineage
-git clone --depth 1 -b lineage-23.2 https://github.com/sapphire-sm6225/android_vendor_lineage.git vendor/lineage
 
 # extract vendor tree
 source vendorextract.sh 2>&1 | tee "extract_log.txt"
