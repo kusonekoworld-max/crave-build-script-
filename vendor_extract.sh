4793dd8d6e951d9d67ae9cde56105226230448c3
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-08-12 08:49:01 +0400

    Update vendor_extract.sh

--- vendor_extract.sh
+++ vendor_extract.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 echo ""
 echo "extracting vendor tree"
+echo ""
 
 # define directories
 DEVICE_TREE="device/xiaomi/creek"
@@ -13,3 +15,7 @@ cd "$DEVICE_TREE" || { echo  "Error: Cannot access device tree folder"; exit 1; 
 # extract the vendor tree
 export PATCHELF=$(which patchelf)
 PYTHONPATH="$TOOLS" python3 ./extract-files.py "$DUMP"
+
+echo ""
+echo "extraction completed"
+echo ""
