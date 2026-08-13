eda8922c4bfab89f46892dfd2d3ed1785f8bf132
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-08-13 16:31:36 +0400

    Remove command to delete soong_namespace file
    Removed the command to delete the conflicting soong_namespace file.

--- start_build.sh
+++ start_build.sh
@@ -28,9 +28,6 @@ curl -sfLo vendorextract.sh -z vendorextract.sh https://raw.githubusercontent.co
 chmod +x vendorextract.sh
 ./vendorextract.sh
 
-# remove conflicting soong_namespace
-rm -rf hardware/qcom-caf/sm6225/data-ipa-cfg-mgr/Android.bp
-
 # setup build env
 source build/envsetup.sh
 
