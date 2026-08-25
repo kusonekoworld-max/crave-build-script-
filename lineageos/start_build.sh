bcae23d46bee7104a753d698715358ffe7b3cf33
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-08-25 09:28:27 +0400

    Update start_build.sh

--- lineageos/start_build.sh
+++ lineageos/start_build.sh
@@ -37,10 +37,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     lineage.updater.uri=${OTA_URL}
 
 # Inherit FastCharge configurations
-$(call inherit-product, packages/apps/FastCharge/fastcharge.mk)
+\$(call inherit-product, packages/apps/FastCharge/fastcharge.mk)
 
 # Inherit FastCharge configurations
-$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+\$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
 EOF
 
 # setup build env
