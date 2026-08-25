9be7e8827a1e8b3e5dc6cb5c21cebadf6d21833f
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-08-25 10:06:37 +0400

    Fix upload script to use correct ZIP file path

--- lineageos/start_build.sh
+++ lineageos/start_build.sh
@@ -66,7 +66,7 @@ ROM_DIR="out/target/product/creek/"
 ZIP_FILE=$(ls "$ROM_DIR" | grep "lineage-.*-creek.zip$" | tail -n 1)
 if [ -n "${ZIP_FILE}" ]; then
     curl -sfLo upload.sh -z upload.sh https://raw.githubusercontent.com/nuruszama/crave/creek/tools/GoFile-upload.sh
-    chmod +x upload.sh ; ./upload.sh "${ZIP_FILE}"
+    chmod +x upload.sh ; ./upload.sh "${ROM_DIR}${ZIP_FILE}"
     echo "upload done!"
 else
     echo "no zip found at out/ dir..."
