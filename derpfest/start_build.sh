3b0e2e74fc67517b1c95f4347752d71b2f7b7fc1
kusonekoworld-max <kusonekoworld@gmail.com>
2026-08-31 03:43:38 +0700

    Create start_build.sh

---
+++ derpfest/start_build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+clear
+
+# Reset all local modifications and delete untracked/generated files
+repo forall -c "git reset --hard HEAD"
+repo forall -c "git clean -fd"
+
+# Maintainer and Host Info
+export BUILD_USERNAME="kusonekoworld"
+export BUILD_HOSTNAME="creek"
+
+# Custom Build Tag
+export TARGET_UNOFFICIAL_BUILD_ID="DerpFest-Edition"
+
+# Build Optimizations & Checks
+export SKIP_ABI_CHECKS=true
+export WITH_DEXPREOPT=true
+
+# remove device tree
+rm -rf .repo/local_manifests
+
+# re-initialize the DerpFest source
+repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16.2 --git-lfs --depth=1
+
+# clone local manifest
+git clone https://github.com/kusonekoworld-max/local_manifests.git -b main --depth=1 .repo/local_manifests
+
+# resync the repo source
+repo sync -j16 --force-sync
+
+# setup build env
+source build/envsetup.sh
+
+# remove intermediates files with seapp
+find out/soong/.intermediates -type d -name "*seapp*" -exec rm -rf {} +
+
+# change modified date to make soong start again
+touch device/xiaomi/creek/BoardConfig.mk
+
+# prepare device menu
+lunch lineage_creek-bp4a-user
+
+# Clean staging dirs
+make installclean
+
+# start building
+mka derp
+
+# Upload
+echo "upload to gofile..."
+ROM_DIR="out/target/product/creek/"
+ZIP_FILE=$(ls "$ROM_DIR" | grep "DerpFest-.*-creek.zip$" | tail -n 1)
+if [ -n "${ZIP_FILE}" ]; then
+    curl -sfLo upload.sh -z upload.sh https://raw.githubusercontent.com/kusonekoworld-max/crave-build-script-/creek/tools/GoFile-upload.sh
+    chmod +x upload.sh ; ./upload.sh "${ROM_DIR}${ZIP_FILE}"
+    echo "upload done!"
+else
+    echo "no zip found at out/ dir..."
+    exit 1
+fi
